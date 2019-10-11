9501397bff54e9a199c11ad6059e14c899045403
Bernard Huff <bernard.huff008@gmail.com>
2019-10-11 11:54:56 -0500

    Added google fonts to the site.

--- functions.php
+++ functions.php
@@ -18,5 +18,13 @@ add_action('init', 'gymfitness_menus');
 // Stylesheets and scripts 
 
 function gymfitness_scripts() {
+
+    // Normalize CSS
+    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+
+    //Google Fonts
+    wp_enqueue_style('googlefont', 'https://fonts.googleapis.com/css?family=Roboto&display=swap', array(), '1.0.0');
     
 }
+
+add_action('wp_enqueue_scripts', 'gymfitness_scripts');
