0f68e6b5038e4a3ae3cc217c7a0c7179874accce
Bernard Huff <bernard.huff008@gmail.com>
2019-10-11 00:40:11 -0500

    Added new social menu

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php 
+
+//Creates the menues
+
+function gymfitness_menus() {
+
+    //Wordpress Function
+    register_nav_menus( array(
+        'main-menu' => 'Main Menu',
+        'social-menu' => 'Social Menu'
+    ) );
+}
+
+// Hook
+
+add_action('init', 'gymfitness_menus');
+
+
