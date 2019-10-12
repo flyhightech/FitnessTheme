22209311f369d7c79bb9e95ae3a3f425031a55d6
Bernard Huff <bernard.huff008@gmail.com>
2019-10-11 19:17:22 -0500

    Added min scripts to the project.

--- footer.php
+++ footer.php
@@ -1,5 +1,24 @@
 
+<footer class="site-footer container">
 
+    <div class="footer-content">
+
+        <?php 
+            
+            $args = array(
+                'theme_location' => 'main-menu',
+                'container' => 'nav',
+                'container_class' => 'footer-menu'
+            );
+
+            wp_nav_menu($args);
+
+        ?>
+
+        <p class="copyright">All Rights Reserved <?php echo get_bloginfo( 'name' ) . " " . date('Y'); ?> </p>
+    </div>
+
+</footer>
 
 </body>
 </html>
